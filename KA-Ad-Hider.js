dfa2d339366bdd8ebfbf371e1c443d81ed95994d
Tienza <Piradon.Liengtiraphan1@gmail.com>
2017-02-11 22:13:46 -0500

    Matches KissCartoon now

--- KA-Ad-Hider.js
+++ KA-Ad-Hider.js
@@ -5,6 +5,7 @@
 // @description  Auto Hides All Ads on KissAnime
 // @author       Piradon (Tien) Liengtiraphan
 // @match        http://kissanime.ru/*
+// @match	 http://kisscartoon.se/*
 // @grant        none
 // ==/UserScript==
 
@@ -25,7 +26,7 @@
         for(var i = 0; i < temp1.length; i++){
             if($(temp1[i]).html() == "Hide")
                 temp1[i].click();
-            else if($(temp1[i]).attr('target') == "_blank" && ($(temp1[i]).html() != "Cartoon list" && $(temp1[i]).html() != "Read Manga" && $(temp1[i]).html() != "Read Novel" && $(temp1[i]).html() != "Watch Drama"))
+            else if($(temp1[i]).attr('target') == "_blank" && ($(temp1[i]).html() != "Cartoon list" && $(temp1[i]).html() != "Read Manga" && $(temp1[i]).html() != "Read Novel" && $(temp1[i]).html() != "Watch Drama" && $(temp1[i]).html() != "Anime list"))
                 temp1[i].remove();
         }
     }, 2000);
